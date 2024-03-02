18f54e288ca100c67013708170b844d4639ef064
davidolejniczak <David.olejniczak@icloud.com>
2024-03-01 23:11:33 -0500

    Removed scanner class and moved all scan functions to reader

--- src/main/java/ca/mcmaster/se2aa4/island/team305/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team305/Explorer.java
@@ -61,8 +61,7 @@ public class Explorer implements IExplorerRaid {
         logger.info("The status of the drone is {}", status);
         JSONObject extraInfo = response.getJSONObject("extras");
         logger.info("Additional information received: {}", extraInfo);
-        Scanner scanner = new Scanner();
-        scanner.processBiomes(extraInfo);
+        readerclass.processBiomes(extraInfo);
         logger.info("E delta = {}", readerclass.getRange("E")); //why is this here
     }
 
