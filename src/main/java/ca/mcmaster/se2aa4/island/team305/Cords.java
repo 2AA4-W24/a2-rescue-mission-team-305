4061265ffc46df73d8f211fbdc95ffc88c896131
davidolejniczak <David.olejniczak@icloud.com>
2024-03-19 12:39:42 -0400

    Fixed error with the cord tracking of heading

--- src/main/java/ca/mcmaster/se2aa4/island/team305/Cords.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team305/Cords.java
@@ -42,33 +42,35 @@ public class Cords {
                 }
             }
         }
-        if (lastmove.getString("action").equals("heading") && lastmove == move) { // i cooked here so it may be burnt and a bit broken
-            JSONObject direction = move.getJSONObject("parameters"); //if some error with heading or cords check here first
-            String headingChange = direction.getString("direction");
-            if (headingChange.equals("N") || headingChange.equals("S") || headingChange.equals("E") || headingChange.equals("W")) {
-                switch (currDirection + "-" + headingChange) {
-                    case "N-W", "W-N": {
-                        EastWest -= 1;
-                        NorthSouth += 1;
-                        break;
-                    }
-                    case "N-E", "E-N": {
-                        EastWest += 1;
-                        NorthSouth += 1;
-                        break;
-                    }
-                    case "S-W", "W-S": {
-                        EastWest -= 1;
-                        NorthSouth -= 1;
-                        break;
-                    }
-                    case "S-E", "E-S": {
-                        EastWest += 1;
-                        NorthSouth -= 1;
-                        break;
-                    }
-                    default: {
-                        logger.info("ERROR in drone cords HEADING move");
+        if (lastmove != null) {//tech debt
+            if (lastmove.getString("action").equals("heading") && lastmove == move) { // i cooked here so it may be burnt and a bit broken
+                JSONObject direction = move.getJSONObject("parameters"); //if some error with heading or cords check here first
+                String headingChange = direction.getString("direction");
+                if (headingChange.equals("N") || headingChange.equals("S") || headingChange.equals("E") || headingChange.equals("W")) {
+                    switch (currDirection + "-" + headingChange) {
+                        case "N-W", "W-N": {
+                            EastWest -= 1;
+                            NorthSouth += 1;
+                            break;
+                        }
+                        case "N-E", "E-N": {
+                            EastWest += 1;
+                            NorthSouth += 1;
+                            break;
+                        }
+                        case "S-W", "W-S": {
+                            EastWest -= 1;
+                            NorthSouth -= 1;
+                            break;
+                        }
+                        case "S-E", "E-S": {
+                            EastWest += 1;
+                            NorthSouth -= 1;
+                            break;
+                        }
+                        default: {
+                            logger.info("ERROR in drone cords HEADING move");
+                        }
                     }
                 }
             }
